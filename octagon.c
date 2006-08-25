d52147e3b0e1b4d3146ee6aa2ae80fecbe12f7db
Scott Feeney <graue@oceanbase.org>
2006-08-25 12:41:44 -0500

    Add an octagon wave generator.
    darcs-hash:20060825174144-bbafa-48eea0a5468260b0c4c551898c640bbe35542d50

---
+++ octagon.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "binary.h"
+#include "defs.h"
+
+/* octagon: generate an octagon wave */
+
+#define CLAMP(min, n, max) ((n)<(min)?(min):((n)>(max)?(max):(n)))
+
+/*
+   _
+ _/ \_
+      \_/
+*/
+
+static float octagon(float input)
+{
+	input = fmod(input, 360.0f);
+	input /= 45.0f;
+	if (input < 1.0f) /* - */
+		return 0.0f;
+	else if (input < 2.0f) /* / */
+		return input - 1.0f;
+	else if (input < 3.0f) /* - */
+		return 1.0f;
+	else if (input < 4.0f) /* \ */
+		return 1.0f - (input - 3.0f);
+	else if (input < 5.0f) /* - */
+		return 0.0f;
+	else if (input < 6.0f) /* \ */
+		return -(input - 5.0f);
+	else if (input < 7.0f) /* - */
+		return -1.0f;
+	else
+		return input - 8.0f;
+}
+
+int main(int argc, char *argv[])
+{
+	float freq = 1000, amp = 0.5f, phase = 0.0f;
+	float period;
+	long len = RATE;
+	long n;
+	int i;
+
+	SET_BINARY_MODE
+
+	/* read options */
+	for (i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-freq") && ++i < argc)
+			freq = atof(argv[i]); /* frequency of the wave */
+		else if (!strcmp(argv[i], "-amp") && ++i < argc)
+			amp = atof(argv[i]);  /* amplitude from 0 to 1 */
+		else if (!strcmp(argv[i], "-len") && ++i < argc)
+			len = atof(argv[i]) / 1000.0f * RATE; /* length in ms */
+		else if (!strcmp(argv[i], "-phase") && ++i < argc)
+			phase = atof(argv[i]); /* phase in degrees */
+		else if (!strcmp(argv[i], "-help"))
+		{
+			fprintf(stderr, "options: -freq arg, -amp arg, -len "
+				"arg, -phase arg\n");
+			exit(0);
+		}
+	}
+
+	/* check options */
+	freq  = CLAMP(0.01f, freq, RATE / 22.0f * 10.f);
+	amp   = CLAMP(0.0f, amp, 1.0f);
+	phase = CLAMP(0.0f, phase, 359.99f);
+
+	/* convert options */
+	amp *= 32768.0f;
+	period = RATE / freq;
+
+	for (n = 0; n < len; n++)
+	{
+		float f;
+
+		f = octagon((n + phase) * 360.0f / period) * amp;
+
+		/* left channel */
+		if (fwrite(&f, sizeof f, 1, stdout) < 1)
+			break;
+
+		/* right channel */
+		if (fwrite(&f, sizeof f, 1, stdout) < 1)
+			break;
+	}
+
+	return 0;
+}
