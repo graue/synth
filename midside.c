aa60c627228276efa8742b57ba9586bd0bcc1209
Scott Feeney <graue@oceanbase.org>
2006-11-09 12:42:15 -0600

    Add LR -> MS program (midside).
    darcs-hash:20061109184215-bbafa-b4f3c26fae1f5d5ef15514a62399a3a609603d28

---
+++ midside.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "synth.h"
+
+/* midside.c: convert left/right to mid/side */
+
+/* TODO: make into a stereo program that also does MS->LR and LR->RL */
+
+static void midside(void);
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	SET_BINARY_MODE
+	for (i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-help"))
+		{
+			fprintf(stderr, "options: none\n");
+			exit(0);
+		}
+	}
+
+	midside();
+	return 0;
+}
+
+static void midside(void)
+{
+	float in[2], out[2];
+
+	for (;;)
+	{
+		if (fread(in, sizeof in[0], 2, stdin) < 1)
+			return;
+
+		out[1] = in[0] - in[1];
+		out[0] = in[0] - out[1]/2;
+
+		if (fwrite(out, sizeof out[0], 2, stdout) < 1)
+			return;
+	}
+}
