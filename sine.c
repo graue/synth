6ad1ae833e0e4c145bec07c056aab99225308b7c
Scott Feeney <graue@oceanbase.org>
2006-11-30 11:45:14 -0600

    sine: Add -modfreq option to modulate frequency.
    darcs-hash:20061130174514-bbafa-07c8068c704176751e8784d89d8f0e3c3414b330

--- sine.c
+++ sine.c
@@ -8,11 +8,12 @@
 
 int main(int argc, char *argv[])
 {
-	float freq = 1000, amp = 0.5f, phase = 0.0f;
-	float period;
+	float freq = 1000.0f, amp = 0.5f, startphase = 0.0f;
+	double phase, inc;
 	long len = RATE;
 	long n;
 	int i;
+	int modfreq = 0; /* use input to modulate frequency */
 
 	/* read options */
 	for (i = 1; i < argc; i++)
@@ -24,31 +25,46 @@ int main(int argc, char *argv[])
 		else if (!strcmp(argv[i], "-len") && ++i < argc)
 			len = atof(argv[i]) / 1000.0f * RATE; /* length in ms */
 		else if (!strcmp(argv[i], "-phase") && ++i < argc)
-			phase = atof(argv[i]); /* phase in degrees */
+			startphase = atof(argv[i]); /* phase in degrees */
+		else if (!strcmp(argv[i], "-modfreq"))
+			modfreq = 1; /* modulate frequency with input */
 		else if (!strcmp(argv[i], "-help"))
 		{
 			fprintf(stderr, "options: -freq arg, -amp arg, -len "
-				"arg, -phase arg\n");
+				"arg, -phase arg, -modfreq\n");
 			exit(0);
 		}
 	}
 
 	/* check options */
-	freq  = CLAMP(0.01f, freq, RATE / 22.0f * 10.f);
-	amp   = CLAMP(0.0f, amp, 1.0f);
-	phase = CLAMP(0.0f, phase, 359.99f);
+	freq = CLAMP(0.01f, freq, RATE / 22.0f * 10.f);
+	amp = CLAMP(0.0f, amp, 1.0f);
+	startphase = CLAMP(0.0f, startphase, 359.99f);
 
 	/* convert options */
 	amp *= 32768.0f;
-	phase = phase * M_PI / 180.0f;
-	period = RATE / freq;
+	startphase = startphase * M_PI / 180.0f;
+
+	phase = startphase;
+	inc = 2*M_PI * freq / RATE;
 
 	SET_BINARY_MODE
 	for (n = 0; n < len; n++)
 	{
 		float f;
 
-		f = sin((n + phase) * 2*M_PI / period) * amp;
+		if (modfreq)
+		{
+			float inputs[2], avg;
+			if (fread(inputs, sizeof inputs[0], 2, stdin) < 2)
+				avg = 0.0f;
+			else
+				avg = (inputs[0] + inputs[1] / 2) / 32768.0f;
+			phase += avg;
+		}
+
+		f = amp * sin(phase);
+		phase += inc;
 
 		/* left channel */
 		if (fwrite(&f, sizeof f, 1, stdout) < 1)
