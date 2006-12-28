31955e14bbabb7ed944ca05b916341a95ae360fb
Scott Feeney <graue@oceanbase.org>
2006-12-27 23:12:17 -0600

    Add mul.c, for multiplying waves together.
    darcs-hash:20061228051217-bbafa-c116f45d172baa37eeefd666227a6ab8266e0854

---
+++ mul.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "synth.h"
+
+/* mul.c: multiply waveforms */
+
+#define CLAMP(min, n, max) ((n)<(min)?(min):((n)>(max)?(max):(n)))
+#define MAXINPUTS 288
+static void mul(FILE **inputs, int numfiles);
+
+int main(int argc, char *argv[])
+{
+	float max = 32768.0f;
+	int i;
+	FILE *inputs[MAXINPUTS];
+	int numfiles = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-amp") && i+1 < argc)
+			max = atof(argv[++i]) * 32768.0f;
+		else if (!strcmp(argv[i], "-help"))
+		{
+			fprintf(stderr, "options: -amp overall_mul\n");
+			fprintf(stderr, "Also list 1 to %d input filenames.\n",
+				MAXINPUTS);
+			exit(0);
+		}
+		else if (argv[i][0] != '-')
+		{
+			if (numfiles == MAXINPUTS)
+			{
+				fprintf(stderr, "mul: too many files\n");
+				return 1;
+			}
+			inputs[numfiles] = fopen(argv[i], "rb");
+			if (inputs[numfiles] == NULL)
+			{
+				fprintf(stderr, "mul: can't open %s\n",
+					argv[i]);
+			}
+			else
+				numfiles++;
+		}
+	}
+
+	if (numfiles == 0)
+	{
+		fprintf(stderr, "mul: no files\n");
+		return 1;
+	}
+
+	SET_BINARY_MODE
+	mul(inputs, numfiles);
+
+	for (i = 0; i < numfiles; i++)
+		fclose(inputs[i]);
+
+	return 0;
+}
+
+static void mul(FILE **inputs, int numfiles)
+{
+	int i;
+	int filedone[MAXINPUTS];
+	int filesleft = numfiles;
+	float f;
+
+	memset(filedone, 0, numfiles * sizeof filedone[0]);
+
+	for (;;)
+	{
+		float product = 1.0f;
+
+		for (i = 0; i < numfiles; i++)
+		{
+			if (filedone[i])
+				continue;
+
+			if (fread(&f, sizeof f, 1, inputs[i]) < 1)
+			{
+				filedone[i] = 1;
+				filesleft--;
+				if (filesleft == 0)
+					return;
+				continue;
+			}
+
+			product *= f;
+		}
+
+		f = product;
+		fwrite(&f, sizeof f, 1, stdout);
+	}
+}
