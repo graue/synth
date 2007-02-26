6bf25b46f71005ad3104b29adbffa4501ddc72fb
Scott Feeney <graue@oceanbase.org>
2007-02-26 02:50:05 -0600

    power.c: Make unsigned by default, add -signed option.
    darcs-hash:20070226085005-bbafa-0a6c61677a5384ce1e26ee9bef6d52427cd1a1d8

--- power.c
+++ power.c
@@ -6,30 +6,33 @@
 
 /* power.c: raise each of a waveform's samples to a power */
 
-static void power(float exponent);
+static void power(float exponent, int signedexp);
 
 int main(int argc, char *argv[])
 {
 	float exponent = 1.0f;
 	int i;
+	int signedexp = 0;
 
 	for (i = 1; i < argc; i++)
 	{
 		if (!strcmp(argv[i], "-exp") && i+1 < argc)
 			exponent = atof(argv[++i]);
+		else if (!strcmp(argv[i], "-signed"))
+			signedexp = 1;
 		else if (!strcmp(argv[i], "-help"))
 		{
-			fprintf(stderr, "options: -exp exponent\n");
+			fprintf(stderr, "options: -exp exponent, -signed\n");
 			exit(0);
 		}
 	}
 
 	SET_BINARY_MODE
-	power(exponent);
+	power(exponent, signedexp);
 	return 0;
 }
 
-static void power(float exponent)
+static void power(float exponent, int signedexp)
 {
 	float f;
 	int positiveexponent = exponent > 0.0f;
@@ -37,7 +40,17 @@ static void power(float exponent)
 
 	while (fread(&f, sizeof f, 1, stdin) == 1)
 	{
-		if (!(!wholeexponent && f < 0.0f)
+		if (!signedexp)
+		{
+			if (positiveexponent || f != 0.0f)
+			{
+				if (f > 0.0f)
+					f = pow(f, exponent);
+				else
+					f = -pow(-f, exponent);
+			}
+		}
+		else if (!(!wholeexponent && f < 0.0f)
 			&& !(!positiveexponent && f == 0.0f))
 		{
 			f = pow(f, exponent);
