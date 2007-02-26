cfe45ccb9cc65d3a3cd3c9fb3d40c2c6b99e8f8d
Scott Feeney <graue@oceanbase.org>
2007-02-26 02:22:30 -0600

    Add power.c, exponentiation.
    darcs-hash:20070226082230-bbafa-dbbc092ed56f07ed4e155ae5b1e6b627eaad7213

---
+++ power.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "synth.h"
+
+/* power.c: raise each of a waveform's samples to a power */
+
+static void power(float exponent);
+
+int main(int argc, char *argv[])
+{
+	float exponent = 1.0f;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-exp") && i+1 < argc)
+			exponent = atof(argv[++i]);
+		else if (!strcmp(argv[i], "-help"))
+		{
+			fprintf(stderr, "options: -exp exponent\n");
+			exit(0);
+		}
+	}
+
+	SET_BINARY_MODE
+	power(exponent);
+	return 0;
+}
+
+static void power(float exponent)
+{
+	float f;
+	int positiveexponent = exponent > 0.0f;
+	int wholeexponent = exponent == floorf(exponent);
+
+	while (fread(&f, sizeof f, 1, stdin) == 1)
+	{
+		if (!(!wholeexponent && f < 0.0f)
+			&& !(!positiveexponent && f == 0.0f))
+		{
+			f = pow(f, exponent);
+		}
+
+		if (fwrite(&f, sizeof f, 1, stdout) < 1)
+			return;
+	}
+}
