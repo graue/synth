884180ff0eff5ff91b6e97e094d5b48749296308
Scott Feeney <graue@oceanbase.org>
2007-07-05 15:43:50 -0500

    stretch: Add linear and drop-sample resampling modes.
    darcs-hash:20070705204350-bbafa-a29a0a61edfbd51df8e6de9dfc0aafca34026d84

--- stretch.c
+++ stretch.c
@@ -8,8 +8,12 @@
 
 static void stretch(void);
 
+enum { SPLINE, LINEAR, DROP }; /* Resampling types. */
+static int resampling_type = SPLINE;
+
 /* The rate is a multiple of normal speed, i.e. 2.0 means 2x as fast. */
 static double rate = 2.0f;
+
 
 int main(int argc, char *argv[])
 {
@@ -19,9 +23,14 @@ int main(int argc, char *argv[])
 	{
 		if (!strcmp(argv[i], "-rate") && i+1 < argc)
 			rate = strtod(argv[++i], NULL);
+		if (!strcmp(argv[i], "-linear"))
+			resampling_type = LINEAR;
+		if (!strcmp(argv[i], "-drop"))
+			resampling_type = DROP;
 		else if (!strcmp(argv[i], "-help"))
 		{
-			fprintf(stderr, "options: -rate speedratio\n");
+			fprintf(stderr, "options: -rate speedratio, "
+				"-linear | -drop\n");
 			exit(0);
 		}
 	}
@@ -45,22 +54,17 @@ static float *nextframe(void)
 }
 
 /* formula copied from Cyanphase MiniTrk (example buzz tracker); CSI? */
-static float interpolate(float minusone, float zero, float one, float two,
-	double fracpart)
+static float interpolate_spline(float minusone, float zero, float one,
+	float two, double fracpart)
 {
 	return ((((3.0f * (zero - one) - minusone + two) * 0.5f * fracpart)
 		+ 2.0f * one + minusone - (5.0f * zero + two) * 0.5f)
 		* fracpart + (one - minusone) * 0.5f) * fracpart + zero;
+}
 
-	/* Other formulas that could be used here:
-	 *
-	 * Linear:
-	 * return fracpart*one + (1.0-fracpart)*zero;
-	 * return zero + fracpart*(one-zero);
-	 *
-	 * Drop-sample:
-	 * return one;
-	 */
+static float interpolate_linear(float zero, float one, float fracpart)
+{
+	return zero + fracpart * (one - zero);
 }
 
 static void stretch(void)
@@ -83,10 +87,23 @@ static void stretch(void)
 			pos -= 1.0f;
 		}
 
-		out[0] = interpolate(buf[0][0], buf[1][0], buf[2][0],
-			buf[3][0], pos);
-		out[1] = interpolate(buf[0][1], buf[1][1], buf[2][1],
-			buf[3][1], pos);
+		if (resampling_type == SPLINE)
+		{
+			out[0] = interpolate_spline(buf[0][0], buf[1][0],
+				buf[2][0], buf[3][0], pos);
+			out[1] = interpolate_spline(buf[0][1], buf[1][1],
+				buf[2][1], buf[3][1], pos);
+		}
+		else if (resampling_type == LINEAR)
+		{
+			out[0] = interpolate_linear(buf[1][0], buf[2][0], pos);
+			out[1] = interpolate_linear(buf[1][1], buf[2][1], pos);
+		}
+		else /* resampling_type == DROP */
+		{
+			out[0] = buf[2][0];
+			out[1] = buf[2][1];
+		}
 
 		if (fwrite(out, sizeof out[0], 2, stdout) < 1)
 			return;
