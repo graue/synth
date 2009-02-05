8f46c7201ede5f1d601c129907ed3ce61de55036
Scott Feeney <graue@oceanbase.org>
2009-02-05 01:26:18 -0600

    delay: Accept a -dryout option.
    darcs-hash:20090205072618-bbafa-ded30d3fa96d769c8166a5e266395a96e024b901

--- delay.c
+++ delay.c
@@ -6,13 +6,14 @@
 
 /* delay.c: a delay line */
 
-static void delay(int len_in_smp, float feedback, float wetout);
+static void delay(int len_in_smp, float feedback, float dryout, float wetout);
 
 int main(int argc, char *argv[])
 {
 	float len = 50.0f;
 	float feedback = 37.5f;
 	float wetout = 100.0f;
+	float dryout = 100.0f;
 	int len_in_smp;
 	int i;
 
@@ -26,10 +27,12 @@ int main(int argc, char *argv[])
 			feedback = atof(argv[++i]);
 		if (!strcmp(argv[i], "-wetout") && i+1 < argc)
 			wetout = atof(argv[++i]);
+		if (!strcmp(argv[i], "-dryout") && i+1 < argc)
+			dryout = atof(argv[++i]);
 		else if (!strcmp(argv[i], "-help"))
 		{
 			fprintf(stderr, "options: -len ms, -feedback "
-				"percent, -wetout percent\n");
+				"percent, -wetout percent, -dryout percent\n");
 			exit(0);
 		}
 	}
@@ -40,11 +43,11 @@ int main(int argc, char *argv[])
 		len_in_smp = 1;
 
 	SET_BINARY_MODE
-	delay(len_in_smp, feedback, wetout);
+	delay(len_in_smp, feedback, wetout, dryout);
 	return 0;
 }
 
-static void delay(int len_in_smp, float feedback, float wetout)
+static void delay(int len_in_smp, float feedback, float wetout, float dryout)
 {
 	float *buf;
 	float f, f0;
@@ -55,6 +58,7 @@ static void delay(int len_in_smp, float feedback, float wetout)
 	/* convert percentages to ratios */
 	feedback /= 100.0f;
 	wetout /= 100.0f;
+	dryout /= 100.0f;
 
 	buf = malloc(sizeof *buf * len_in_smp);
 	if (buf == NULL)
@@ -67,7 +71,7 @@ static void delay(int len_in_smp, float feedback, float wetout)
 
 	while (fread(&f0, sizeof f0, 1, stdin) == 1)
 	{
-		f = f0 + wetout*buf[bufpos];
+		f = dryout*f0 + wetout*buf[bufpos];
 
 		if (fwrite(&f, sizeof f, 1, stdout) < 1)
 			return;
