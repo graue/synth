f5e6c4abbeae0c33f2daea635321b8001a2d4bfc
Scott Feeney <scott@oceanbase.org>
2010-06-18 13:48:10 -0500

    gate: Use threshdB, drop only with option, otherwise zero out.
    Ignore-this: e10a6b52f621f7558522633f650df4b4
    
    darcs-hash:20100618184810-c78a8-f1d5a59cded3f3fd54ee5aca47813967852304d2

--- gate.c
+++ gate.c
@@ -11,17 +11,20 @@
  * You give this program a time in milliseconds, and a sample
  * magnitude value, mag, with 1.0 being full-scale. If every
  * sample has a magnitude less than mag for the time you give,
- * then all samples after that are dropped until one with
+ * then all samples after that are zeroed out until one with
  * larger or equal magnitude compared to mag is found.
+ *
+ * or with -drop the samples are just dropped.
  */
 
-static void gate(float len, float mag);
+static void gate(float len, float threshdB, int drop);
 
 int main(int argc, char *argv[])
 {
 	float len = 50.0f;
-	float mag = 0.00000001f;
+	float threshdB = -80.0f;
 	int i;
+	int drop = 0;
 
 	get_rate();
 
@@ -29,36 +32,48 @@ int main(int argc, char *argv[])
 	{
 		if (strcmp(argv[i], "-len") == 0 && i+1 < argc)
 			len = atof(argv[++i]);
-		else if (strcmp(argv[i], "-mag") == 0 && i+1 < argc)
-			mag = atof(argv[++i]);
+		else if (strcmp(argv[i], "-threshdB") == 0 && i+1 < argc)
+			threshdB = atof(argv[++i]);
+		else if (strcmp(argv[i], "-drop") == 0)
+			drop = 1;
 		else if (!strcmp(argv[i], "-help"))
 		{
-			fprintf(stderr, "options: -len ms, -mag sampval\n");
+			fprintf(stderr, "options: -len ms, -threshdB -dB, \n"
+				"-drop\n");
 			exit(0);
 		}
 	}
 
 	SET_BINARY_MODE
-	gate(len, mag);
+	gate(len, threshdB, drop);
 	return 0;
 }
 
-static void gate(float len, float mag)
+#define M_LN10_OVER_20 0.115129254649702284200899573
+#define DBTORAT(x) exp((x) * M_LN10_OVER_20)
+
+static void gate(float len, float threshdB, int drop)
 {
 	float f;
 	int smplen = (int)(RATE * len / 1000.0f);
 	int numlow = 0;
 	int closed = 0;
+	float mag = DBTORAT(threshdB);
 
 	while (fread(&f, sizeof f, 1, stdin) == 1)
 	{
 		if (closed)
 		{
-			if (fabsf(f) < mag)
-				continue;
-			closed = 0;
+			if (fabs(f) > mag)
+				closed = 0;
+			else
+			{
+				f = 0.0;
+				if (drop)
+					continue;
+			}
 		}
-		else if (fabsf(f) < mag && ++numlow == smplen)
+		else if (fabs(f) < mag && ++numlow == smplen)
 		{
 			numlow = 0;
 			closed = 1;
