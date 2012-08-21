ccfcd357d4b20cb280198d060126077b99e9d988
Scott Feeney <scott@oceanbase.org>
2012-08-21 01:23:25 -0500

    fix: Verbose option prints number of NaNs, infs

--- fix.c
+++ fix.c
@@ -6,35 +6,59 @@
 
 /* fix.c: fix NANs and infinities */
 
-static void fix(void);
+static void fix(int verbose);
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, verbose = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (strcmp(argv[i], "-help") == 0)
+		if (strcmp(argv[i], "-verbose") == 0)
+			verbose = 1;
+		else if (strcmp(argv[i], "-help") == 0)
 		{
-			fprintf(stderr, "options: none\n");
+			fprintf(stderr, "options: -verbose\n");
 			exit(0);
 		}
 	}
 
 	SET_BINARY_MODE
-	fix();
+	fix(verbose);
 	return 0;
 }
 
-static void fix(void)
+static void fix(int verbose)
 {
 	float f;
+	unsigned long nans = 0, plusinfs = 0, minusinfs = 0, total = 0;
 
 	while (fread(&f, sizeof f, 1, stdin) == 1)
 	{
 		if (!isfinite(f))
+		{
 			f = 0.0f;
+			if (verbose) /* Keep stats on infinities and NaNs. */
+			{
+				if (f == INFINITY)
+					plusinfs++;
+				else if (f == -INFINITY)
+					minusinfs++;
+				else
+					nans++;
+
+				if (!total)
+					fprintf(stderr, "Non-finite number "
+						"encountered\n");
+
+				total++;
+			}
+		}
 		if (fwrite(&f, sizeof f, 1, stdout) < 1)
-			return;
+			break;
 	}
+
+	if (verbose)
+		fprintf(stderr, "fix: %lu +Infs, %lu -Infs, %lu NaNs\n",
+			plusinfs, minusinfs, nans);
 }
